40f8085483f8f3527ca6a06c3bdc7bac9a67ccc0
Kit Plummer <kit@ditto.live>
2023-08-16 10:10:57 -0400

    subscription change to message produced

---
+++ README.md
@@ -0,0 +1,13 @@
+# example service for taking collection changes and producing messages on a RedPanda topic
+
+Need an `.env` with this things populated:
+
+```
+APP_ID=
+APP_TOKEN=
+RAW_COLLECTION_NAME=
+PRODUCT_COLLECTION_NAME=
+RAW_TOPIC_NAME=
+PRODUCT_TOPIC_NAME=
+```
+
--- index.ts
+++ index.ts
@@ -1,15 +1,24 @@
-import { init, Ditto, TransportConfig } from '@dittolive/ditto'
+import { init, Ditto, Document, TransportConfig } from '@dittolive/ditto'
 require('dotenv').config()
 
 let ditto
 
-let subscription
+let rawSubscription
+let rawLiveQuery
+let rawDocuments: Document[] = []
+
+let productSubscription
+let productLiveQuery
+let productDocuments: Document[] = []
 
 let APP_ID = process.env.APP_ID
 //let OFFLINE_TOKEN = process.env.OFFLINE_TOKEN
 //let SHARED_KEY = process.env.SHARED_KEY
 let APP_TOKEN = process.env.APP_TOKEN
-let COLLECTION_NAME = process.env.COLLECTION_NAME
+let RAW_COLLECTION_NAME = process.env.RAW_COLLECTION_NAME
+let PRODUCT_COLLECTION_NAME = process.env.PRODUCT_COLLECTION_NAME
+let RAW_TOPIC_NAME = process.env.RAW_TOPIC_NAME
+let PRODUCT_TOPIC_NAME = process.env.PRODUCT_TOPIC_NAME
 
 const { Kafka, CompressionTypes, logLevel } = require('kafkajs')
 const ip = require('ip')
@@ -44,15 +53,61 @@ async function main() {
      }
    })
 
-	ditto.setTransportConfig(config)
+  ditto.setTransportConfig(config)
 
+  const producer = kafka.producer()
+  await producer.connect()
   ditto.startSync()
   
-  subscription = ditto.store.collection(COLLECTION_NAME).find("isDeleted == false").subscribe()
+  rawSubscription = ditto.store.collection(RAW_COLLECTION_NAME).find("synced == false").subscribe()
+  rawLiveQuery = ditto.store
+    .collection(RAW_COLLECTION_NAME)
+    .find("synced == false")
+    .observeLocal((docs, event) => {
+      rawDocuments = docs
+      let rawDoc
+      rawDocuments.map((rawDoc) => {
+        // Send to RedPanda topic
+        producer.send({
+          topic: RAW_TOPIC_NAME,
+          messages: [
+            { payload: rawDoc.value }
+          ],
+        })
+        rawDoc.value.synced = true
+        // Set synced, and evict
+        ditto.store.collection(RAW_COLLECTION_NAME).upsert(rawDoc)
+        ditto.store.collection(RAW_COLLECTION_NAME).evict(rawDoc)
+      })
+    })
+
+  productSubscription = ditto.store.collection(PRODUCT_COLLECTION_NAME).find("synced == false").subscribe()
+  productLiveQuery = ditto.store
+    .collection(PRODUCT_COLLECTION_NAME)
+    .find("synced == false")
+    .observeLocal((docs, event) => {
+      productDocuments = docs
+      let productDoc
+      productDocuments.map((rawDoc) => {
+        producer.send({
+          topic: PRODUCT_TOPIC_NAME,
+          messages: [
+            { payload: productDoc.value }
+          ],
+        })
+        productDoc.value.synced = true
+        // Set synced, and evict
+        ditto.store.collection(PRODUCT_COLLECTION_NAME).upsert(productDoc)
+        ditto.store.collection(PRODUCT_COLLECTION_NAME).evict(productDoc)
+      })
+    })
 
   const presenceObserver = ditto.presence.observe((graph) => {
     console.log("local peer connections: ", graph.localPeer.connections)
+    if (graph.localPeer.connections.length != 0) {
+    }
   })
+
 }
 
 main()
